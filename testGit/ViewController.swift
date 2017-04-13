63ee82543e351746d629210e1fddba168593dac6
Zach Radford <radfz8638@zpr-l0007084.local>
2017-04-13 10:05:55 -0400

    Added label and button to storyboard and view controller. Label shows number of times button has been pressed

--- testGit/ViewController.swift
+++ testGit/ViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var number = 0
+    @IBOutlet weak var buttonCounter: UILabel!
+    
+    @IBAction func button(_ sender: UIButton) {
+        number += 1
+        buttonCounter.text = String(number)
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
